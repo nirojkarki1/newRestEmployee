893d51ea0bc40fd71a560c13438864e27a6a6479
nirojkarki1 <nirojkarki85@gmail.com>
2023-04-16 17:35:50 -0500

    Changed

--- src/main/java/com/javaguides/demoSpringBoot/Dao/StudentDao.java
+++
@@ -1,9 +0,0 @@
-package com.javaguides.demoSpringBoot.Dao;
-
-import com.javaguides.demoSpringBoot.Entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentDao  extends JpaRepository<Student,Integer> {
-}
